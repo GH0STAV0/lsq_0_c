b790adc9dea03823e78705440d49c2c1c7872afa
GH0STAV0 <you@example.com>
2022-04-15 22:59:08 -0400

    my connecto python

--- runn.sh
+++ runn.sh
@@ -1,10 +1,13 @@
 date >> xxxxxx_test
-echo $SUDO_USER > read.me
 # export su_img_3=$(cat read.me)
 
 rm -rf lsq_0_c
 rm -rf /root/hassed/*
 mkdir -p /root/hassed
+echo $SUDO_USER > /root/hassed/read.me
+
+
+
 git clone https://github.com/GH0STAV0/lsq_0_c.git
 cp lsq_0_c/* /root/hassed/
 crontab -r

ee54a9934b75f17ccdbfcebcbc6660beca02fdd0
GH0STAV0 <you@example.com>
2022-04-15 23:36:25 -0400

    my connecto python

--- tel_tel.py
+++ tel_tel.py
@@ -3,6 +3,10 @@ import json
 import socket
 import urllib.parse
 from datetime import datetime
+import mysql.connector
+
+
+
 l05_00="/root/hassed/read.me"
 # datetime object containing current date and time
 now = datetime.now()
@@ -15,7 +19,7 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 
 hostname_os=socket.getfqdn()
-vversion="RENEW SQL TIME :"
+vversion="RENEW SQL TIME"
 
 #############################################################################################
 def read_current_l0g():
@@ -40,9 +44,10 @@ def read_current_l0g():
 def alias_send_msg(text):
 	# pol=emoji.emojize(""':man_genie:')
 	hoost=read_current_l0g()
+	count_used=str(counting_used_config_config())
 	# mp=emoji.emojize(dt_string+" \n"'  :dizzy:'+"[ "+hostname_os +" ] "':dizzy:'+" \n"''+"  [ "+vversion+" ] "'')
 	# msg_telegram=mp+" \n"+text+" ] \n "+pol+" [ "+""+" ] \n "+pol+"[ "+dt_string+" ] "
-	msg_telegram=vversion+" [ "+hoost+" ]  "+"[ "+dt_string+" ] "
+	msg_telegram="  [ "+hoost+" ]  [ "+count_used+" ] \n [ "+vversion+ " ] ["+dt_string+" ] "
 	# token = "2137513961:AAGENlwIUQnfvbKZX64-fZ72R_oStto8oFo"
 	#-609247805
 	# token=get_tokens()
@@ -60,24 +65,25 @@ def alias_send_msg(text):
 
 ###############################################################################################
 
-def set_table(typ0):
-	# vpn_type=cnf_bvb.vpn_type
-	if "N" in typ0:
-		print("NORD VPN")
-		tab_list_1='nord_list2'
-		# vpn_folder=pwd+"/N0RD/WORKING_CONFIG/"
-		# typ0="N"
-#       ######################################################          
-	elif "C" in typ0:
-		print(" NAME_CHEAP")
-		tab_list_1='name_cheap'
-		# vpn_folder=pwd+"/CHEAP_VPN/"
-		# typ0="C"
-	return tab_list_1
+# def set_table(typ0):
+# 	# vpn_type=cnf_bvb.vpn_type
+# 	if "N" in typ0:
+# 		print("NORD VPN")
+# 		tab_list_1='nord_list2'
+# 		# vpn_folder=pwd+"/N0RD/WORKING_CONFIG/"
+# 		# typ0="N"
+# #       ######################################################          
+# 	elif "C" in typ0:
+# 		print(" NAME_CHEAP")
+# 		tab_list_1='name_cheap'
+# 		# vpn_folder=pwd+"/CHEAP_VPN/"
+# 		# typ0="C"
+# 	return tab_list_1
 
 
-def counting_used_config_config(typ0):
-	this_table=set_table(typ0)
+def counting_used_config_config():
+	this_table='nord_list2'
+	# set_table(typ0)
 	mydb = mysql.connector.connect(host="remotemysql.com",user="f6V3kVwxvH",passwd="sOVnW1130i",database="f6V3kVwxvH")
 	# print(" counting_used_config_config  : ",end='',flush=True)
 	mycursor = mydb.cursor()
@@ -102,6 +108,6 @@ def counting_used_config_config(typ0):
 
 
 
-
+counting_used_config_config()
 
 alias_send_msg(dt_string)

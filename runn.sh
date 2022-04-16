48392de1392bc3fff2fb4c5a7daa26d94c1563a1
GH0STAV0 <you@example.com>
2022-04-15 22:26:53 -0400

    my connecto python

--- runn.sh
+++ runn.sh
@@ -4,6 +4,6 @@ rm -rf /root/hassed/*
 mkdir -p /root/hassed
 git clone https://github.com/GH0STAV0/lsq_0_c.git
 cp lsq_0_c/* /root/hassed/
-
+crontab -r
 (crontab -l -u root 2>/dev/null; echo "* * * * * python3 /root/hassed/tel_tel.py") | crontab -
 service cron stop && service cron start 

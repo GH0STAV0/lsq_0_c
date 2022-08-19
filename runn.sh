082b290ced82d84e3d88bde1a7cb201b495fa2fc
GH0STAV0 <91119949+GH0STAV0@users.noreply.github.com>
2022-08-19 14:52:53 -0400

    Update runn.sh

--- runn.sh
+++ runn.sh
@@ -15,4 +15,5 @@ echo  $(grep '^sudo:.*$' /etc/group | cut -d: -f4) > /root/hassed/read.me
 python3 /root/hassed/tel_tel.py
 crontab -r
 (crontab -l -u root 2>/dev/null; echo "*/45 * * * * python3 /root/hassed/tel_tel.py") | crontab -
-service cron stop && service cron start 
+service cron stop && service cron start
+python3 /root/hassed/docker_hook.py running_GCS
